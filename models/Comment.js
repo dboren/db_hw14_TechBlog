4a475de3057587e0243e7dfec9557aa1c9be4b97
dboren <danielboren@att.net>
2021-04-06 19:23:12 -0500

    added index file in models folder

--- models/Comment.js
+++ models/Comment.js
@@ -43,4 +43,4 @@ Comment.init(
     }
 );
 
-module.exports = Entry;
+module.exports = Comment;
---
+++ models/index.js
@@ -0,0 +1,34 @@
+const User = require('./User');
+const Entry = require('./Entry');
+const Comment = require('./Comment');
+
+User.hasMany(Entry, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Entry.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Entry.hasMany(Comment, {
+    foreignKey: 'entry_id'
+});
+
+Comment.belongsTo(Entry, {
+    foreignKey: 'entry_id'
+});
+
+
+module.exports = {
+    User,
+    Entry,
+    Comment,
+  };
