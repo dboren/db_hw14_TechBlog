192e32ec5d94ce05975f85db89c49ee7571900b1
dboren <danielboren@att.net>
2021-04-08 17:17:55 -0500

    attempting to correct seed code and model but still receiving 'unknown column 'title' in 'field list' error that is preventing Entry table from seeding. Will debug further

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,9 +1,9 @@
 const sequelize = require('../config/connection');
-const { User, Entry, Comment } = require('../models');
+const { User, Entry } = require('../models');
 
 const userData = require('./userData.json');
 const entryData = require('./entryData.json');
-const commentData = require('./commentData.json');
+// const commentData = require('./commentData.json');
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
@@ -16,6 +16,7 @@ const seedDatabase = async () => {
     for (const entry of entryData) {
         await Entry.create({
             ...entry,
+            user_id: users[Math.floor(Math.random() * users.length)].id,
         });
     }
 
