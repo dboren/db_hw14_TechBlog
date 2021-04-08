8a82bc452e96669b382399856f2db66a589f86d0
dboren <danielboren@att.net>
2021-04-08 17:33:26 -0500

    removed duplicate line from server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const path = require('path');
 const session = require('express-session');
-const path = require('path');
+
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
