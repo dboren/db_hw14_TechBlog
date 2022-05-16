7fb1281e697a2ce8fde533946d368d85bc313d7c
Daniel Boren <danielboren52@gmail.com>
2022-05-15 21:05:25 -0500

    reformatted login handler without initial if wrapper. Getting error 'cannot access PasswordEl' before initialization.' Is this because of the encryption?

--- public/JS/login.js
+++ public/JS/login.js
@@ -1,33 +1,56 @@
-const loginFormHandler = async (event) => {
+
+const loginFormHandler = async function(event) {
     event.preventDefault();
-
-    const email = document.querySelector('#email-login').value.trim();
-    console.log('email:', email);
-    const password = document.querySelector('#password-login').value.trim();
-    console.log('password:', password);
-
-    if (email && password) {
-        const response = await fetch('/api/user/login', {
-            method: 'POST',
-            body: JSON.stringify({ 
-                email: email.value,
-                password: password.value 
-            }),
-            headers: { 'Content-Type': 'application/json' },
-        });
-        console.log('response:', response);
-
-        if (response.ok) {
-            // document.location.replace('/dashboard');
-            console.log('form submitted');
-            console.log('logged in: ', req.session.user_id);
-        } else {
-            alert(response.statusText);
-            console.log('something went wrong!')
-            // console.log('logged in: ', req.session.user_id);
-        }
-    }
-};
-
-
+  
+    const emailEl = document.querySelector('#email-login').value.trim();
+    console.log('email:', emailEl);
+    const passwordEl = document.querySelector('#password-login').value.
+    fetch("/api/user/login", {
+      method: "post",
+      body: JSON.stringify({
+        email: emailEl.value,
+        password: passwordEl.value
+      }),
+      headers: { "Content-Type": "application/json" }
+    })
+      .then(function() {
+        document.location.replace("/dashboard");
+      })
+      .catch(err => console.log(err));
+  };
+  
 document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+
+// const loginFormHandler = async (event) => {
+//     event.preventDefault();
+
+//     const email = document.querySelector('#email-login').value.trim();
+//     console.log('email:', email);
+//     const password = document.querySelector('#password-login').value.trim();
+//     console.log('password:', password);
+
+//     if (email && password) {
+//         const response = await fetch('/api/user/login', {
+//             method: 'POST',
+//             body: JSON.stringify({ 
+//                 email: email.value,
+//                 password: password.value 
+//             }),
+//             headers: { 'Content-Type': 'application/json' },
+//         });
+//         console.log('response:', response);
+
+//         if (response.ok) {
+//             // document.location.replace('/dashboard');
+//             console.log('form submitted');
+//             console.log('logged in: ', req.session.user_id);
+//         } else {
+//             alert(response.statusText);
+//             console.log('something went wrong!')
+//             // console.log('logged in: ', req.session.user_id);
+//         }
+//     }
+// };
+
+
+// document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
